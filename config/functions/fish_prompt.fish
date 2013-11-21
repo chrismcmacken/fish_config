2490af88edcf8f8992be290895038d4fd887a89c
Chris McMacken <chris.mcmacken@gmail.com>
2013-11-21 16:42:33 -0600

    Updating my prompt

--- config/functions/fish_prompt.fish
+++ config/functions/fish_prompt.fish
@@ -1,17 +1,11 @@
 function fish_prompt --description 'Write out the prompt'
-	
 	set -l last_status $status
 
 	if not set -q __fish_prompt_normal
 		set -g __fish_prompt_normal (set_color normal)
 	end
 
-	# PWD
-	set_color $fish_color_cwd
-	echo -n (prompt_pwd)
-	set_color normal
-
-	printf '%s ' (__fish_git_prompt)
+	printf '[%s@%s:%s%s%s %s] ' $USER (hostname -s) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) (__fish_git_prompt)
 
 	if not test $last_status -eq 0
 	set_color $fish_color_error
